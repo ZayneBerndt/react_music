10a7d20f4fefadfe5f3158fc3398cf6ee41fe9a8
Zayne  Berndt <berndt.zayne@gmail.com>
2019-04-02 00:31:56 +0100

    added song controls

--- src/components/Album.js
+++ src/components/Album.js
@@ -9,21 +9,50 @@ class Album extends Component {
             return album.slug === this.props.match.params.slug
           });
           this.state = {
-            album: album
-          };
-      }
+                album: album,
+                currentSong: album.songs[0],
+                isPlaying: false
+            };
+          this.audioElement = document.createElement('audio');
+          this.audioElement.src = album.songs[0].audioSrc;
+        }
+        play() {
+            this.audioElement.play();
+            this.setState({ isPlaying: true });
+        }
+        pause() {
+            this.audioElement.pause();
+            this.setState({ isPlaying: false });
+        }
+        setSong(song) {
+            this.audioElement.src = song.audioSrc;
+            this.setState({ currentSong: song });
+            
+        }
+
     render () {
         return (
         <section className="album">
-             <section id="album-info">
-             <img id="album-cover-art" />
-             <div className="album-details">
-             <h1 id="album-title"></h1>
-             <h2 className="artist"></h2>
-             <div id="release-info"></div>
-           </div>
-            </section>
-            </section>
+            <section id="album-info">
+            <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+        </section>
+            <table id="song-list">
+            <colgroup >
+             <col id="song-number-column" />
+             <col id="song-title-column" />
+             <col id="song-duration-column" />
+            </colgroup>  
+           <tbody >
+               
+            {this.state.album.songs.title}
+            {this.state.album.songs.title}
+            {this.state.album.songs.title}
+           </tbody>
+            </table>
+        </section>
         )
     }
 }
